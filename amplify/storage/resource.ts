31bb31fc243d5ab65f7f21470ca8af6fe9b97a42
Felix Bubbar <felix@bubbar.dev>
2025-08-14 15:08:21 -0700

    fix storage type

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,30 +2,5 @@ import { defineStorage } from '@aws-amplify/backend'
 import { aws_s3 as s3 } from 'aws-cdk-lib'
 
 export const storage = defineStorage({
-  name: 'onetdata',
-  access: (allow) => ({
-    read: allow.public(),
-    write: allow.authenticated()
-  }),
-  customCdkProps: {
-    blockPublicAccess: new s3.BlockPublicAccess({
-      blockPublicAcls: false,
-      blockPublicPolicy: false,
-      ignorePublicAcls: false,
-      restrictPublicBuckets: false
-    }),
-    cors: [
-      {
-        allowedMethods: [
-          s3.HttpMethods.GET,
-          s3.HttpMethods.PUT,
-          s3.HttpMethods.POST,
-          s3.HttpMethods.DELETE,
-          s3.HttpMethods.HEAD
-        ],
-        allowedOrigins: ['*'],
-        allowedHeaders: ['*']
-      }
-    ]
-  }
+  name: 'onetdata'
 })
